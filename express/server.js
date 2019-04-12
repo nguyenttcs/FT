a68aca6af81f2eadca9c0eaba1935720e757c7bc
ntran57 <ntran57@csc.com>
2019-04-12 09:47:48 +0700

    FT_Express: render ejs, set view ejs, use static file, introduce ejs

--- express/server.js
+++ express/server.js
@@ -1,1 +1,16 @@
 const express = require('express')
+const app = express()
+
+app.set('view engine','ejs')
+app.use(express.static('public'))
+
+app.get('/file',(req,res)=>{
+    const number = 12
+    res.render('b',{number})
+})
+
+const port = process.env.PORT||3000
+app.listen(port,()=>{
+    console.log('Server started on port '+port)
+})
+
