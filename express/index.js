070ec9af82f07a08c4f5c6598f04051c8f9ac8f6
ntran57 <ntran57@csc.com>
2019-04-12 08:57:44 +0700

    FT_Express: Declare Express, ejs, how to start server on port, create a rout get, install JSONViewer

--- express/index.js
+++ express/index.js
@@ -5,3 +5,20 @@
  * yarn init -y
  * yarn add express ejs
  */
+
+ const express = require('express')
+ const ejs = require('ejs')
+
+ const app = express()
+
+ app.get('/',(req,res)=>{
+     res.send({
+         name:"Nguyen",
+         height: 160
+     })
+ })
+
+ const port = process.env.PORT||3000
+ app.listen(port,()=>{
+     console.log("Server started on port "+port)
+ })
