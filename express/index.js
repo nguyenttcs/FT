58f31ed746e9554f221b0c1a63316da99fde7e6a
ntran57 <ntran57@csc.com>
2019-04-12 09:10:41 +0700

    FT_Express: send file html, send status, get dirname

--- express/index.js
+++ express/index.js
@@ -18,6 +18,21 @@
      })
  })
 
+ app.get('/user/:id',(req,res)=>{
+     const id = req.params.id
+     const type = typeof id
+     res.send({
+         id,
+         type
+     })
+ })
+
+ app.get('/a',(req,res)=>{
+    //res.sendStatus(404)
+    //console.log(__dirname);
+    res.sendFile(__dirname+'/views/a.html');
+})
+
  const port = process.env.PORT||3000
  app.listen(port,()=>{
      console.log("Server started on port "+port)
