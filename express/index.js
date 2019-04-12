97676c55334870d5772c6c535b89cb3e8284366e
ntran57 <ntran57@csc.com>
2019-04-12 09:31:29 +0700

    FT_Express: class, eval, get, params

--- express/index.js
+++ express/index.js
@@ -33,6 +33,33 @@
     res.sendFile(__dirname+'/views/a.html');
 })
 
+app.get('/:so_a/:tenpheptinh/:so_b',(req,res)=>{
+    const {so_a,so_b,tenpheptinh} = req.params
+    const tinh = new Tinh(so_a,tenpheptinh,so_b)
+    res.send(tinh.getResult())
+})
+
+class Tinh{
+    constructor(so_a,tenpheptinh,so_b){
+        this.so_a = so_a
+        this.so_b = so_b
+        this.tenpheptinh=tenpheptinh
+    }
+
+    get pheptinh(){
+        if(this.tenpheptinh=='cong') return '+'
+        else if(this.tenpheptinh=='tru') return '-'
+        else if(this.tenpheptinh=='nhan') return '*'
+        return '/'
+    }
+
+    getResult(){
+        const result = `${this.so_a} ${this.pheptinh} ${this.so_b}`
+        return `${result} = ${eval(result)}`
+re
+    }
+}
+
  const port = process.env.PORT||3000
  app.listen(port,()=>{
      console.log("Server started on port "+port)
