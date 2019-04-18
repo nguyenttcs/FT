220f77c2078536d7e683188d9c02fbd51d964d31
ntran57 <ntran57@csc.com>
2019-04-18 11:03:14 +0700

    FT_ejs: add body-parser, add method POST for form, create post for add-student

--- ejs/index.js
+++ ejs/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 app.set('view engine','ejs')
 
+const bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false })) //string||array
+
 const {Student, arrStudent} = require('./models/Student')
 
 app.get('/',(req,res)=>{
@@ -14,6 +18,14 @@ app.get('/demo',(req,res)=>{
 
 app.get('/add',(req,res)=>{
     res.render('add')
+})
+
+//yarn add body-parser
+app.post('/add-student',(req,res)=>{
+    const {txtName, txtAvatar, txtLink} = req.body
+    res.send({
+        txtName, txtAvatar, txtLink
+    })
 })
 
 const port = process.env.PORT||3000
